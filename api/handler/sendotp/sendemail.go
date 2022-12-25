ed559e85ecd8b9776415099a03999853fff4fdcf
Hariharan148 <harirevorhustle@gmail.com>
2022-12-25 13:24:30 +0530

    rate limit added

--- api/handler/sendotp/sendemail.go
+++ api/handler/sendotp/sendemail.go
@@ -1,156 +1,127 @@
 package sendotp
 
 import (
-	"github.com/Hariharan148/hustlie-email-api/api/config/emailApi"
-	"github.com/Hariharan148/hustlie-email-api/api/config/db"
-	mailjet "github.com/mailjet/mailjet-apiv3-go"
-	"github.com/go-redis/redis/v8"
 	"crypto/rand"
+	"encoding/json"
 	"fmt"
 	"log"
-	"net/http"
-	"net"
-	"strings"
-	"strconv"
 	"os"
+	"strconv"
 	"time"
 
+	"github.com/Hariharan148/Url-Shortener-Go-Redis/api/database"
+	"github.com/Hariharan148/hustlie-email-api/api/config/db"
+	"github.com/Hariharan148/hustlie-email-api/api/config/emailApi"
+	"github.com/go-redis/redis/v8"
+	"github.com/gofiber/fiber/v2"
+	mailjet "github.com/mailjet/mailjet-apiv3-go"
+	"github.com/tidwall/gjson"
 )
 
-var ip string
-
-
-func getIP(r *http.Request) (string, error) {
-
-    //Get IP from the X-REAL-IP header
-	
-    ip = r.Header.Get("X-REAL-IP")
-	
-    netIP := net.ParseIP(ip)
-    if netIP != nil {
-		fmt.Println("1",ip)
-        return ip, nil
-    }
-
-    //Get IP from X-FORWARDED-FOR header
-    ips := r.Header.Get("X-FORWARDED-FOR")
-	
-    splitIps := strings.Split(ips, ",")
-    for _, ip := range splitIps {
-        netIP := net.ParseIP(ip)
-        if netIP != nil {
-			fmt.Println("2",ips)
-            return ip, nil
-        }
-    }
-
-    //Get IP from RemoteAddr
-	
-    ip, _, err := net.SplitHostPort(r.RemoteAddr)
-    if err != nil {
-        return "", err
-    }
-    netIP = net.ParseIP(ip)
-    if netIP != nil {
-		fmt.Println(r.RemoteAddr)
-        return ip, nil
-    }
-    return "", fmt.Errorf("No valid ip found")
+type Request struct {
+	Email string `json:"email"`
+	Name  string `json:"name"`
 }
 
-var r1 = db.RedisClient(1)
+type Response struct {
+	MailStatus      string        `json:"mail_status"`
+	DbStatus        string        `json:"db_status"`
+	XRateLimit      int           `json:"x_rate_limit"`
+	XRateLimitReset time.Duration `json:"x_rate_limit_reset"`
+}
 
+func rateLimiter(c *fiber.Ctx) error {
+	rd1 := db.RedisClient(1)
+	defer rd1.Close()
 
+	val, err := rd1.Get(db.Ctx, c.IP()).Result()
 
-func rateLimiter(r *http.Request)(string, error){
-
-	defer r1.Close() 
-
-	ip, err := getIP(r)
-	fmt.Println(ip)
-	if err != nil {
-		log.Printf("Error getting the IP addr: ", err)
-		return "", err
-	}
-
-	val, err := r1.Get(db.Ctx, ip).Result()
-	if err != nil && err != redis.Nil{
-		log.Printf("Error cant connect to db: ", err)
-		return "", err
-
-	} else if err == redis.Nil{
-		err = r1.Set(db.Ctx, ip, os.Getenv("API_LIMIT"), 24 *3600 *time.Second).Err()
-		if err != nil{
-			log.Printf("Error cant connect to db: ", err)
-			return "", err
-		}
+	if err == redis.Nil {
+		_ = rd1.Set(db.Ctx, c.IP(), os.Getenv("API_LIMIT"), 24*3600*time.Second).Err()
 	} else {
-		val, err = r1.Get(db.Ctx, ip).Result()
-		valInt, err := strconv.Atoi(val)
-		if err != nil {
-			log.Printf("Error while converting to int: ", err)
-			return "", err
-		}
+		// val, _ = rd1.Get(database.Ctx, c.IP()).Result()
+		valInt, _ := strconv.Atoi(val)
 		if valInt <= 0 {
-			return "true", nil
+			limit, _ := rd1.TTL(database.Ctx, c.IP()).Result()
+			return c.Status(fiber.StatusServiceUnavailable).JSON(fiber.Map{
+				"error":              "Too many login attempts! You are restricted for next 24hrs",
+				"x_rate_limit_reset": limit / time.Nanosecond / time.Minute,
+			})
 		}
-		fmt.Println("lmit", valInt )
-	
-		
 	}
-	
-	return "", nil
+	return nil
 }
 
+func SendEmail(c *fiber.Ctx) error {
+	r1 := db.RedisClient(1)
+	defer r1.Close()
 
-func SendEmail(name string, email string, r *http.Request)( *mailjet.ResultsV31, string, error){
+	var body *Request
 
-	limit, err:= rateLimiter(r)
-	if err != nil {
-		return &mailjet.ResultsV31{}, "Error while rate limiting", err
+	if err := c.BodyParser(&body); err != nil {
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{"error": "cannot parse the body"})
 	}
 
-	if limit != "" {
-		return &mailjet.ResultsV31{}, "Too many login attempts! You are restricted for next 24hrs", nil
-	}
+	rateLimiter(c)
 
 	client := emailApi.Client()
 
 	otp := otpGenerator()
 
-	formatedMsg := fmt.Sprintf("<h3>Here is your OTP - " + otp + "</h3><br />Dear " + name +", Welcome to Huslie!")
+	formatedMsg := fmt.Sprintf("<h3>Here is your OTP - " + otp + "</h3><br />Dear " + body.Name + ", Welcome to Huslie!")
 
-	messagesInfo := []mailjet.InfoMessagesV31 {
-      mailjet.InfoMessagesV31{
-        From: &mailjet.RecipientV31{
-          Email: "harirevorhustle@gmail.com",
-          Name: "Hustlie",
-        },
-        To: &mailjet.RecipientsV31{
-          mailjet.RecipientV31 {
-            Email: email,
-            Name: name,
-          },
-        }, 
-        Subject: "OTP - Team Hustlie",
-        TextPart: "Here is your OTP - " + otp,
-        HTMLPart:  formatedMsg,
-      },
+	messagesInfo := []mailjet.InfoMessagesV31{
+		mailjet.InfoMessagesV31{
+			From: &mailjet.RecipientV31{
+				Email: "harirevorhustle@gmail.com",
+				Name:  "Hustlie",
+			},
+			To: &mailjet.RecipientsV31{
+				mailjet.RecipientV31{
+					Email: body.Email,
+					Name:  body.Name,
+				},
+			},
+			Subject:  "OTP - Team Hustlie",
+			TextPart: "Here is your OTP - " + otp,
+			HTMLPart: formatedMsg,
+		},
 	}
 
-
-	messages := mailjet.MessagesV31{Info: messagesInfo }
+	messages := mailjet.MessagesV31{Info: messagesInfo}
 	res, err := client.SendMailV31(&messages)
 	if err != nil {
-		return res, otp, err
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+			"error": "error while sending the email!",
+		})
 	}
 
-	r1.Decr(db.Ctx, ip)
+	data, err := json.MarshalIndent(res, "", "    ")
+	if err != nil {
+		fmt.Printf("Error while marshaling payload: ", err)
+	}
 
-	return res, otp, nil
+	strData := string(data[:])
+
+	msgStatus := gjson.Get(strData, "Messages.0.Status")
+
+	resp := Response{
+		MailStatus:      msgStatus.String(),
+		DbStatus:        "",
+		XRateLimit:      2,
+		XRateLimitReset: 24,
+	}
+
+	r1.Decr(db.Ctx, c.IP())
+
+	limit, _ := r1.Get(database.Ctx, c.IP()).Result()
+	resp.XRateLimit, _ = strconv.Atoi(limit)
+
+	limitReset, _ := r1.TTL(database.Ctx, c.IP()).Result()
+	resp.XRateLimitReset = limitReset / time.Nanosecond / time.Minute
+
+	return c.Status(fiber.StatusOK).JSON(res)
 }
-
-
 
 func otpGenerator() string {
 
@@ -163,7 +134,3 @@ func otpGenerator() string {
 	return strOtp
 
 }
-
-
-
-
--- main.go
+++ main.go
@@ -1,75 +1,31 @@
 package main
 
 import (
-	"net/http"
-	"fmt"
-	"encoding/json"
 	"log"
+	"os"
+
 	"github.com/Hariharan148/hustlie-email-api/api/handler/sendotp"
-	"github.com/tidwall/gjson"
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/logger"
+	"github.com/joho/godotenv"
 )
 
-type Request struct {
-	Email string 
-	Name  string 
+func setUpRoutes(app *fiber.App) {
+
+	app.Post("/sendotp", sendotp.SendEmail)
+	// app.Get("/verifyotp", verifyotp)
 }
 
-type Response struct{
-	MailStatus string `json:"mail_status"`
-	DbStatus string `json:"db_status"`
-}
-
-
-func parseBody(r *http.Request)Request{
-	var body Request
-	err := json.NewDecoder(r.Body).Decode(&body)
+func main() {
+	err := godotenv.Load(".env")
 	if err != nil {
-		log.Printf("error %v",err)
-	}
-	return body
-}
-
-
-func response(w http.ResponseWriter, r *http.Request){
-	if r.URL.Path != "/otp" {
-		http.Error(w, "404 not found", http.StatusNotFound)
+		log.Printf("Error loading environment variables ", err)
 	}
 
-	if r.Method != "POST" {
-		http.Error(w, "Method not supported", http.StatusNotFound)
-	}
+	app := fiber.New()
 
-	body := parseBody(r)
+	app.Use(logger.New())
 
-
-	res, otp, err := sendotp.SendEmail(body.Name, body.Email, r)
-	if err != nil {
-		http.Error(w, "Error while sending mail", http.StatusInternalServerError)
-	}
-	
-	data, err:= json.MarshalIndent(res, "", "    ")
-	if err != nil {
-		fmt.Printf("Error while marshaling payload: ", err)
-	}
-
-	strData := string(data[:])
-
-	status := gjson.Get(strData, "Messages.0.Status")
-
-
-	response := Response{
-		MailStatus: status.String(),
-		DbStatus: otp,
-	}
-
-	w.Header().Set("Content-Type", "application/json")
-
-	json.NewEncoder(w).Encode(response)
-
-}
-
-func main(){
-	http.HandleFunc("/sendotp", response)
-	fmt.Println("Server starting at port 8000...")
-	log.Fatal(http.ListenAndServe(":8000", nil ))
+	setUpRoutes(app)
+	log.Fatal(app.Listen(os.Getenv("APP_PORT")))
 }
